318a735d6d3c81ca3e3c5e400dfafb361fa12a27
JesusOfCarrots <tristangapa@gmail.com>
2024-03-09 14:33:34 +0100

    Adding basics

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 import * as THREE from 'three';
 
 let FOV = 75;
-let width = window.innerWidth;
-let height = window.innerHeight;
+let width = window.innerWidth * 0.7;
+let height = window.innerHeight * 0.7;
+let cameraDistance = 5;
 
 let rotationSpeed = 0.02;
 let canRotate = true;
 
 const scene = new THREE.Scene();
-const cam = new THREE.PerspectiveCamera(FOV, width / height, 0.1, 100)
+const cam = new THREE.PerspectiveCamera(FOV, width / height, 0.1, 1000)
+const otherSpace = document.getElementById('other');
 
 const renderer = new THREE.WebGLRenderer();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -16,14 +18,19 @@ const material = new THREE.MeshBasicMaterial( {color:0xffd700} );
 
 const cube = new THREE.Mesh(geometry, material);
 
-
 function setUp(){
     renderer.setSize(width, height);
+    renderer.domElement.classList.add('cav');
     document.body.appendChild(renderer.domElement);
 
     scene.add(cube);
+    // Center
+    let offsetY = window.innerHeight - height;
+    let offsetX = window.innerWidth - width;
+    renderer.domElement.style.marginTop = offsetY / 2 + 'px';
+    otherSpace.style.width  = offsetX + 'px';
 
-    cam.position.z = 5;
+    cam.position.z = cameraDistance;
 
     render();
 }
